8f402e5e105a5b57a3746d6489f4a927fcf55633
ALTESSE Atelier <atelier@altesse-couture.com>
2026-05-24 17:39:27 +0800

    Fix login scrypt verification for Vercel

--- api/login.js
+++ api/login.js
@@ -1,10 +1,17 @@
 const { readData, send, getBody } = require('./_utils');
+const crypto = require('crypto');
+
+function verifyPassword(password, stored) {
+  const [salt, hash] = stored.split(':');
+  if (!salt || !hash) return false;
+  return hash === crypto.scryptSync(password, salt, 64).toString('hex');
+}
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') return send(res, 405, { error: 'Method not allowed' });
   const { username, password } = await getBody(req);
   const users = readData('users');
-  const user = users.find(u => u.username === username && u.password === password);
-  if (!user) return send(res, 401, { error: 'Invalid credentials' });
+  const user = users.find(u => u.username === username);
+  if (!user || !verifyPassword(password, user.password)) return send(res, 401, { error: 'Invalid credentials' });
   send(res, 200, { token: 'altesse_vercel_token', role: user.role });
 };
